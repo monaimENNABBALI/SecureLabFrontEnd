8a3734161f961dba9735b318fcee1446ff70a816
Hiba77 <77454833+Hiba77@users.noreply.github.com>
2022-08-04 14:48:19 +0100

    Add files via upload

---
+++ src/views/rgpd/rgpdaccomp.vue
@@ -0,0 +1,149 @@
+<template lang="">
+    <Intro bgUrl="background-image: url('images/intro-rgpdaudit.jpg');">
+      <div class="title">
+          <h3>By SecureLabs</h3>
+      </div>
+      <div class="content">
+          <h4><i>RGPD Accompagnement</i></h4>
+      </div>
+      <div class="action">
+          <a href="#choiceUs">discover now</a>
+      </div>
+    </Intro>
+    <main>
+        <div class="page-content">
+
+                <div class="container">
+                    <div id="first" class="container mt-6" style="height: 100vh;">
+                        <div class="heading pt-4">
+                            <h3 class="heading-title">RGPD Accompagnement</h3>
+                            <p class="heading-cat">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
+                            </p>
+
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-lg-3 col-sm-4">
+                                <div class="icon-box icon-box-sm text-center p-5" style="background-color: #fff;">
+                                    <span class="icon-box-icon ">
+                                        <i class="icon-puzzle-piece" style="font-size: 10rem;"></i>
+                                    </span>
+                                    <div class="icon-box-content">
+                                        <h3 class="icon-box-title" >element 1</h3><!-- End .icon-box-title -->
+                                    </div><!-- End .icon-box-content -->
+                                </div><!-- End .icon-box -->
+                            </div><!-- End .col-lg-4 col-sm-6 -->
+                            <div class="col-lg-3 col-sm-4">
+                                <div class="icon-box icon-box-sm text-center p-5" style="background-color: #fff;">
+                                    <span class="icon-box-icon ">
+                                        <i class="icon-puzzle-piece" style="font-size: 10rem;"></i>
+                                    </span>
+                                    <div class="icon-box-content">
+                                        <h3 class="icon-box-title" >element 2</h3><!-- End .icon-box-title -->
+                                    </div><!-- End .icon-box-content -->
+                                </div><!-- End .icon-box -->
+                            </div><!-- End .col-lg-4 col-sm-6 -->
+                            <div class="col-lg-3 col-sm-4">
+                                <div class="icon-box icon-box-sm text-center p-5" style="background-color: #fff;">
+                                    <span class="icon-box-icon ">
+                                        <i class="icon-puzzle-piece" style="font-size: 10rem;"></i>
+                                    </span>
+                                    <div class="icon-box-content">
+                                        <h3 class="icon-box-title" >element 3</h3><!-- End .icon-box-title -->
+                                    </div><!-- End .icon-box-content -->
+                                </div><!-- End .icon-box -->
+                            </div><!-- End .col-lg-4 col-sm-6 -->
+                            <div class="col-lg-3 col-sm-4">
+                                <div class="icon-box icon-box-sm text-center p-5" style="background-color: #fff;">
+                                    <span class="icon-box-icon ">
+                                        <i class="icon-puzzle-piece" style="font-size: 10rem;"></i>
+                                    </span>
+                                    <div class="icon-box-content">
+                                        <h3 class="icon-box-title" >element 4</h3><!-- End .icon-box-title -->
+                                    </div><!-- End .icon-box-content -->
+                                </div><!-- End .icon-box -->
+                            </div><!-- End .col-lg-4 col-sm-6 -->
+
+                        </div>
+                    <!-- first -->
+                    </div>
+                    <div style="height:100vh;" class="py-5 my-5">
+                        <h2 class="text-center heading-title my-5 ">La méthodologie d'accompagnement</h2><!-- End .title -->
+                        <div class="accordion accordion-rounded" id="accordion-1">
+                            <div class="card card-box card-sm bg-light">
+                                <div class="card-header" id="heading-1">
+                                    <h2 class="card-title">
+                                        <a role="button" data-toggle="collapse" href="#collapse-1" aria-expanded="true" aria-controls="collapse-1">
+                                            method 1
+                                        </a>
+                                    </h2>
+                                </div><!-- End .card-header -->
+                                <div id="collapse-1" class="collapse show" aria-labelledby="heading-1" data-parent="#accordion-1">
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
+                                    </div><!-- End .card-body -->
+                                </div><!-- End .collapse -->
+                            </div><!-- End .card -->
+
+                            <div class="card card-box card-sm bg-light">
+                                <div class="card-header" id="heading-2">
+                                    <h2 class="card-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse-2" aria-expanded="false" aria-controls="collapse-2">
+                                            method 2
+                                        </a>
+                                    </h2>
+                                </div><!-- End .card-header -->
+                                <div id="collapse-2" class="collapse" aria-labelledby="heading-2" data-parent="#accordion-1">
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
+                                    </div><!-- End .card-body -->
+                                </div><!-- End .collapse -->
+                            </div><!-- End .card -->
+
+                            <div class="card card-box card-sm bg-light">
+                                <div class="card-header" id="heading-3">
+                                    <h2 class="card-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse-3" aria-expanded="false" aria-controls="collapse-3">
+                                            method 3
+                                        </a>
+                                    </h2>
+                                </div><!-- End .card-header -->
+                                <div id="collapse-3" class="collapse" aria-labelledby="heading-3" data-parent="#accordion-1">
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
+                                    </div><!-- End .card-body -->
+                                </div><!-- End .collapse -->
+                            </div><!-- End .card -->
+                            <div class="card card-box card-sm bg-light">
+                                <div class="card-header" id="heading-3">
+                                    <h2 class="card-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse-4" aria-expanded="false" aria-controls="collapse-3">
+                                            method 4
+                                        </a>
+                                    </h2>
+                                </div><!-- End .card-header -->
+                                <div id="collapse-4" class="collapse" aria-labelledby="heading-4" data-parent="#accordion-1">
+                                    <div class="card-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
+                                    </div><!-- End .card-body -->
+                                </div><!-- End .collapse -->
+                            </div><!-- End .card -->
+
+                            
+                        </div><!-- End .accordion -->
+                    </div>
+                </div><!-- End .container -->
+            </div><!-- End .page-content -->
+    </main>
+</template>
+<script>
+import Intro from '../common/Intro'
+export default {
+    components:{
+        Intro
+    }
+}
+</script>
+<style lang="">
+    
+</style>
