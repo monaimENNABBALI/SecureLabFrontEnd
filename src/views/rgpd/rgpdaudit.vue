d64055fbee6c5173d174d6f7c14e1fe38406f859
Hiba77 <77454833+Hiba77@users.noreply.github.com>
2022-08-04 15:03:59 +0100

    Update rgpdaudit.vue

--- src/views/rgpd/rgpdaudit.vue
+++ src/views/rgpd/rgpdaudit.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <Intro bgUrl="background-image: url('images/intro-rgpdaudit.jpg');">
       <div class="title">
-          <h3>SecureLabs vous offre</h3>
+          <h3>By SecureLabs</h3>
       </div>
       <div class="content">
           <h4><i>Les audits de conformité RGPD</i></h4>
