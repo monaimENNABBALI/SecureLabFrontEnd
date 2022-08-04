0a612e1cd5d3525352bbafd731600d25eceaf198
Hiba77 <77454833+Hiba77@users.noreply.github.com>
2022-08-04 04:48:23 +0100

    Create rgpdaudit.vue

---
+++ src/views/rgpd/rgpdaudit.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <Intro bgUrl="background-image: url('images/homeIntro.jpg');">
+      <div class="title">
+          <h3>SecureLabs vous offre</h3>
+      </div>
+      <div class="content">
+          <h4><i>Les audits de conformité RGPD</i></h4>
+      </div>
+      <div class="action">
+          <a href="#choiceUs">discover now</a>
+      </div>
+    </Intro>
+    <div>
+        it's rgpdaudit vue
+    </div>
+</template>
+<script>
+import Intro from '../common/Intro'
+export default {
+    components:{
+        Intro
+    }
+}
+</script>
+<style lang="">
+    
+</style>
