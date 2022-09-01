4f14a5478db065deb8a2e51761a51949c2a67d1a
Hiba77 <77454833+Hiba77@users.noreply.github.com>
2022-09-01 12:50:42 +0100

    Update rgpddpoex.vue

--- src/views/rgpd/rgpddpoex.vue
+++ src/views/rgpd/rgpddpoex.vue
@@ -23,7 +23,7 @@
                                
                             <p> 
                                 Le Règlement Général sur la Protection des Données (RGPD) exige la tenue d’un registre des traitements de données. Ce registre intègre l’ensemble de la documentation interne portant sur les traitements de données et permet d’avoir une vision globalisée sur les écarts de conformité aux obligations réglementaires.
-SECURELABS vous déleste de cette étape pénible en construisant, pour vous, le registre des traitements de données à caractère personnel. Outre notre expertise en la matière, nous vous apportons une véritable valeur ajoutée via l’utilisation de notre outil (pour plus d’informations [redirection vers l’onglet logiciel]).
+SECURELABS vous déleste de cette étape pénible en construisant, pour vous, le registre des traitements de données à caractère personnel. Outre notre expertise en la matière, nous vous apportons une véritable valeur ajoutée via l’utilisation de notre outil .
 La cartographie des traitements sera donc réalisée via une série d’entretiens avec chaque responsable opérationnel dans le but d’identifier les finalités du traitement, les catégories de données traitées et les personnes concernées, les destinataires et les durées de conservation.
 A l’issue de ces entretiens, SECURELABS vous livrera votre registre des traitements données (cartographie) et un plan d’action correctif des écarts de conformité.
 
