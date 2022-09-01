249a0706fbb620d1d87a322f712beb01b35266f0
Hiba77 <77454833+Hiba77@users.noreply.github.com>
2022-09-01 11:26:24 +0100

    Update rgpddpoex.vue

--- src/views/rgpd/rgpddpoex.vue
+++ src/views/rgpd/rgpddpoex.vue
@@ -7,7 +7,7 @@
           <h4>DPO/DPD externe</h4>
       </div>
       <div class="action">
-          <a href="#choiceUs">discover now</a>
+          <a href="#choiceUs">La fonction de DPO, un gain de temps considérable dans sa gouvernance RGPD</a>
       </div>
     </Intro>
     <main>
@@ -17,121 +17,34 @@
                     <div id="first" class="container mt-6" style="height: 100vh;">
                         <div class="heading pt-4">
                             <h3 class="heading-title">DPO/DPD externe</h3>
-                            <p class="heading-cat">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
+                            <p>
+                                La fonction de DPO, un gain de temps considérable dans sa gouvernance RGPD
+                                Le Délégué à la protection des données (DPO) est le chef d’orchestre de la mise en conformité au RGPD : 
+                                Il accompagne le responsable du traitement dans la mise en oeuvre des différentes obligations imposées par le RGPD ; 
+                                Permet de bénéficier d’une flexibilité dans la mise en place du RGPD ; 
+                                Définit et assure le suivi du registre ainsi que de la cartographie des traitements de données ; 
+                                Sensibilise les collaborateurs ; 
+                                Assiste les métiers à la réalisation des analyses d’impact (PIA) ; 
+                                Gère les relations avec la CNIL ; 
+                                Réalise une veille juridique ; 
+                                Participe aux comités de direction ; 
+                                La fonction de DPO est obligatoire pour les responsables et sous-traitants : 
+                                du secteur public ; 
+                                ayant mis en œuvre un traitement à grande échelle exigeant un suivi régulier et systématique des personnes concernées ;
+                                ayant mis en œuvre un traitement à grande échelle contenant des catégories particulières de données ; </p>
+                               <h3 class="heading-title">Le DPO externe - un allié de confiance </h3> 
+                            <p> 
+                                Le Délégué à la protection des données est chargé d’assurer le pilotage de la conformité RGPD. L’externaliser sera bénéfique pour votre entreprise, vous permettra de libérer des ressources et d’apporter une réelle valeur ajoutée via un accompagnement d’expert.
+                                Pour assurer ses missions de DPO, SECURELABS dispose à la fois de compétences juridiques (protection des données) et de compétences en gouvernance de la sécurité des données.
+                                La désignation du DPO sera donc obligatoire dans les cas cités ci-dessus et nous pouvons vous accompagner dans ce sens.
+
                             </p>
 
                         </div>
-                        <div class="row justify-content-center">
-                            <div class="col-lg-3 col-sm-4">
-                                <div class="icon-box icon-box-sm text-center p-5" style="background-color: #fff;">
-                                    <span class="icon-box-icon ">
-                                        <i class="icon-puzzle-piece" style="font-size: 10rem;"></i>
-                                    </span>
-                                    <div class="icon-box-content">
-                                        <h3 class="icon-box-title" >element 1</h3><!-- End .icon-box-title -->
-                                    </div><!-- End .icon-box-content -->
-                                </div><!-- End .icon-box -->
-                            </div><!-- End .col-lg-4 col-sm-6 -->
-                            <div class="col-lg-3 col-sm-4">
-                                <div class="icon-box icon-box-sm text-center p-5" style="background-color: #fff;">
-                                    <span class="icon-box-icon ">
-                                        <i class="icon-puzzle-piece" style="font-size: 10rem;"></i>
-                                    </span>
-                                    <div class="icon-box-content">
-                                        <h3 class="icon-box-title" >element 2</h3><!-- End .icon-box-title -->
-                                    </div><!-- End .icon-box-content -->
-                                </div><!-- End .icon-box -->
-                            </div><!-- End .col-lg-4 col-sm-6 -->
-                            <div class="col-lg-3 col-sm-4">
-                                <div class="icon-box icon-box-sm text-center p-5" style="background-color: #fff;">
-                                    <span class="icon-box-icon ">
-                                        <i class="icon-puzzle-piece" style="font-size: 10rem;"></i>
-                                    </span>
-                                    <div class="icon-box-content">
-                                        <h3 class="icon-box-title" >element 3</h3><!-- End .icon-box-title -->
-                                    </div><!-- End .icon-box-content -->
-                                </div><!-- End .icon-box -->
-                            </div><!-- End .col-lg-4 col-sm-6 -->
-                            <div class="col-lg-3 col-sm-4">
-                                <div class="icon-box icon-box-sm text-center p-5" style="background-color: #fff;">
-                                    <span class="icon-box-icon ">
-                                        <i class="icon-puzzle-piece" style="font-size: 10rem;"></i>
-                                    </span>
-                                    <div class="icon-box-content">
-                                        <h3 class="icon-box-title" >element 4</h3><!-- End .icon-box-title -->
-                                    </div><!-- End .icon-box-content -->
-                                </div><!-- End .icon-box -->
-                            </div><!-- End .col-lg-4 col-sm-6 -->
-
-                        </div>
+                       
                     <!-- first -->
                     </div>
-                    <div style="height:100vh;" class="py-5 my-5">
-                        <h2 class="text-center heading-title my-5 ">La méthodologie de nos DPO/DPD externe</h2><!-- End .title -->
-                        <div class="accordion accordion-rounded" id="accordion-1">
-                            <div class="card card-box card-sm bg-light">
-                                <div class="card-header" id="heading-1">
-                                    <h2 class="card-title">
-                                        <a role="button" data-toggle="collapse" href="#collapse-1" aria-expanded="true" aria-controls="collapse-1">
-                                            method 1
-                                        </a>
-                                    </h2>
-                                </div><!-- End .card-header -->
-                                <div id="collapse-1" class="collapse show" aria-labelledby="heading-1" data-parent="#accordion-1">
-                                    <div class="card-body">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
-                                    </div><!-- End .card-body -->
-                                </div><!-- End .collapse -->
-                            </div><!-- End .card -->
-
-                            <div class="card card-box card-sm bg-light">
-                                <div class="card-header" id="heading-2">
-                                    <h2 class="card-title">
-                                        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse-2" aria-expanded="false" aria-controls="collapse-2">
-                                            method 2
-                                        </a>
-                                    </h2>
-                                </div><!-- End .card-header -->
-                                <div id="collapse-2" class="collapse" aria-labelledby="heading-2" data-parent="#accordion-1">
-                                    <div class="card-body">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
-                                    </div><!-- End .card-body -->
-                                </div><!-- End .collapse -->
-                            </div><!-- End .card -->
-
-                            <div class="card card-box card-sm bg-light">
-                                <div class="card-header" id="heading-3">
-                                    <h2 class="card-title">
-                                        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse-3" aria-expanded="false" aria-controls="collapse-3">
-                                            method 3
-                                        </a>
-                                    </h2>
-                                </div><!-- End .card-header -->
-                                <div id="collapse-3" class="collapse" aria-labelledby="heading-3" data-parent="#accordion-1">
-                                    <div class="card-body">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
-                                    </div><!-- End .card-body -->
-                                </div><!-- End .collapse -->
-                            </div><!-- End .card -->
-                            <div class="card card-box card-sm bg-light">
-                                <div class="card-header" id="heading-3">
-                                    <h2 class="card-title">
-                                        <a class="collapsed" role="button" data-toggle="collapse" href="#collapse-4" aria-expanded="false" aria-controls="collapse-3">
-                                            method 4
-                                        </a>
-                                    </h2>
-                                </div><!-- End .card-header -->
-                                <div id="collapse-4" class="collapse" aria-labelledby="heading-4" data-parent="#accordion-1">
-                                    <div class="card-body">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra imperdiet sem. Donec ligula metus, pharetra et dictum non, fermentum ac lacus. Integer ac magna vitae mauris placerat iaculis vel a risus. Maecenas feugiat eget lorem facilisis venenatis. Nullam sodales tortor in magna ultricies, quis sagittis ante eleifend. Praesent quis.
-                                    </div><!-- End .card-body -->
-                                </div><!-- End .collapse -->
-                            </div><!-- End .card -->
-
-                            
-                        </div><!-- End .accordion -->
-                    </div>
+                    
                 </div><!-- End .container -->
             </div><!-- End .page-content -->
     </main>
