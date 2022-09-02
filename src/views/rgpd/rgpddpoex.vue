6cb0a52be43685821d146a69853dc004aa56fa56
Hiba77 <77454833+Hiba77@users.noreply.github.com>
2022-09-02 13:54:47 +0100

    Update rgpddpoex.vue

--- src/views/rgpd/rgpddpoex.vue
+++ src/views/rgpd/rgpddpoex.vue
@@ -14,17 +14,17 @@
         <div class="page-content">
 
                 <div class="container">
-                    <div id="first" class="container mt-6" style="height: 60vh;">
+                    <div id="first" class="container mt-6" style="height: 80vh;">
                         <div class="heading pt-4">
                         
                             <h3 class="mb-5 title-color-primary" style="font-weight: 600;">Cartographie des traitements de données</h3>
            
                          
                                
-                            <p> 
-                                Le Règlement Général sur la Protection des Données (RGPD) exige la tenue d’un registre des traitements de données. Ce registre intègre l’ensemble de la documentation interne portant sur les traitements de données et permet d’avoir une vision globalisée sur les écarts de conformité aux obligations réglementaires.
-SECURELABS vous déleste de cette étape pénible en construisant, pour vous, le registre des traitements de données à caractère personnel. Outre notre expertise en la matière, nous vous apportons une véritable valeur ajoutée via l’utilisation de notre outil .
-La cartographie des traitements sera donc réalisée via une série d’entretiens avec chaque responsable opérationnel dans le but d’identifier les finalités du traitement, les catégories de données traitées et les personnes concernées, les destinataires et les durées de conservation.
+                            <p class="text-big"> 
+                                Le Règlement Général sur la Protection des Données (RGPD) exige la tenue d’un registre des traitements de données. Ce registre intègre l’ensemble de la documentation interne portant sur les traitements de données et permet d’avoir une vision globalisée sur les écarts de conformité aux obligations réglementaires.<br><br>
+SECURELABS vous déleste de cette étape pénible en construisant, pour vous, le registre des traitements de données à caractère personnel. Outre notre expertise en la matière, nous vous apportons une véritable valeur ajoutée via l’utilisation de notre outil . <br><br>
+La cartographie des traitements sera donc réalisée via une série d’entretiens avec chaque responsable opérationnel dans le but d’identifier les finalités du traitement, les catégories de données traitées et les personnes concernées, les destinataires et les durées de conservation. <br><br>
 A l’issue de ces entretiens, SECURELABS vous livrera votre registre des traitements données (cartographie) et un plan d’action correctif des écarts de conformité.
 
 
